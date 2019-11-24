5033edc041ffaeb4032c009c0445e1c2880c2f26
ikemanep1 <ike.esquivelpilloud@gmail.com>
2019-11-24 13:58:59 -0800

    complete 4th test

--- __tests__/tests.js
+++ __tests__/tests.js
@@ -3,18 +3,18 @@ import { Storage } from './../src/class.js';
 describe ( 'Storage', () => {
   const collectionTest = new Storage(21, 100);
 // RED
-test('should successfully convert user age into their age on Mercury', () => {
+test('should successfully convert user age into their age on venus', () => {
   collectionTest.addAge(10, 10);
-    expect(collectionTest.mercuryAge).toEqual(87);
+    expect(collectionTest.venusAge).toEqual(33);
   });
 // GREEN
-test('should successfully convert user age into their age on Mercury', () => {
+test('should successfully convert user age into their age on venus', () => {
   collectionTest.addAge(21, 100);
-    expect(collectionTest.mercuryAge).toEqual(87);
+    expect(collectionTest.venusAge).toEqual(33);
   });
 // REFACTOR
-test('should successfully convert user age into their age on Mercury', () => {
+test('should successfully convert user age into their age on venus', () => {
   collectionTest.addAge(21, 100);
-    expect(collectionTest.mercuryAge).toEqual(87);
+    expect(collectionTest.venusAge).toEqual(33);
   });
 });
