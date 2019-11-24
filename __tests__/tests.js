7c83060751f7986124e49802709f111d737be5d7
ikemanep1 <ike.esquivelpilloud@gmail.com>
2019-11-24 14:03:51 -0800

    complete 6th test

--- __tests__/tests.js
+++ __tests__/tests.js
@@ -3,18 +3,18 @@ import { Storage } from './../src/class.js';
 describe ( 'Storage', () => {
   const collectionTest = new Storage(21, 100);
 // RED
-test('should successfully convert user age into their age on mars', () => {
+test('should successfully convert user age into their age on jupiter', () => {
   collectionTest.addAge(10, 10);
-    expect(collectionTest.marsAge).toEqual(11);
+    expect(collectionTest.jupiterAge).toEqual(1);
   });
 // GREEN
-test('should successfully convert user age into their age on mars', () => {
+test('should successfully convert user age into their age on jupiter', () => {
   collectionTest.addAge(21, 100);
-    expect(collectionTest.marsAge).toEqual(11);
+    expect(collectionTest.jupiterAge).toEqual(1);
   });
 // REFACTOR
-test('should successfully convert user age into their age on mars', () => {
+test('should successfully convert user age into their age on jupiter', () => {
   collectionTest.addAge(21, 100);
-    expect(collectionTest.marsAge).toEqual(11);
+    expect(collectionTest.jupiterAge).toEqual(1);
   });
 });
