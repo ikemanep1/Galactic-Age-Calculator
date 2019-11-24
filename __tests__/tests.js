8a9eeaace3d8b51e2783dcff1b8358f8afc71055
ikemanep1 <ike.esquivelpilloud@gmail.com>
2019-11-24 15:00:35 -0800

    add expectancy tests

--- __tests__/tests.js
+++ __tests__/tests.js
@@ -2,19 +2,53 @@ import { Storage } from './../src/class.js';
 
 describe ( 'Storage', () => {
   const collectionTest = new Storage(21, 100);
-// RED
-test('should successfully convert user age into their age on jupiter', () => {
-  collectionTest.addAge(10, 10);
+  
+  test('should successfully make age containing what is taken from the 1stinput', () => {
+    let collectionTest = new Storage(21);
+    expect(collectionTest.age).toEqual(21);
+  });
+
+  test('should successfully make expectancy containing what is taken from the 2ndinput', () => {
+    let collectionTest = new Storage(100);
+    expect(collectionTest.expectancy).toEqual(100);
+  });
+
+  test('should successfully convert user age into their age on Mercury', () => {
+    collectionTest.addAge(21, 100);
+    expect(collectionTest.mercuryAge).toEqual(87);
+  });
+
+  test('should successfully convert user age into their age on venus', () => {
+    collectionTest.addAge(21, 100);
+    expect(collectionTest.venusAge).toEqual(33);
+  });
+
+  test('should successfully convert user age into their age on mars', () => {
+    collectionTest.addAge(21, 100);
+    expect(collectionTest.marsAge).toEqual(11);
+  });
+
+  test('should successfully convert user age into their age on jupiter', () => {
+    collectionTest.addAge(21, 100);
     expect(collectionTest.jupiterAge).toEqual(1);
   });
-// GREEN
-test('should successfully convert user age into their age on jupiter', () => {
-  collectionTest.addAge(21, 100);
-    expect(collectionTest.jupiterAge).toEqual(1);
+
+  test('should successfully convert user age expectancy into their age expectancy on mercury', () => {
+    collectionTest.addExpectancy(21, 100);
+    expect(collectionTest.mercuryExpectancy).toEqual(329);
   });
-// REFACTOR
-test('should successfully convert user age into their age on jupiter', () => {
-  collectionTest.addAge(21, 100);
-    expect(collectionTest.jupiterAge).toEqual(1);
+
+  test('should successfully convert user age expectancy into their age expectancy on venus', () => {
+    collectionTest.addExpectancy(21, 100);
+    expect(collectionTest.venusExpectancy).toEqual(128);
   });
-});
+
+  test('should successfully convert user age expectancy into their age expectancy on mars', () => {
+    collectionTest.addExpectancy(21, 100);
+    expect(collectionTest.marsExpectancy).toEqual(42);
+  });
+
+  test('should successfully convert user age expectancy into their age expectancy on jupiter', () => {
+    collectionTest.addExpectancy(21, 100);
+    expect(collectionTest.jupiterExpectancy).toEqual(7);
+  });
